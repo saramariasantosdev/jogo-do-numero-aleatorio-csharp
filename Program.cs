5da20983aa61ed22160944c4a7047f745eb157ba
Sara Maria <saramariasantosdev@gmail.com>
2025-07-27 19:09:31 -0300

    Commit Inicial

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class JogoNumeroAleatorio
+{
+    public static void Main(string[] args)
+    {
+        int minimo = 1;
+        int maximo = 10;
+
+        Random random = new Random();
+        int numeroAleatorio = random.Next(minimo, maximo);
+
+        do
+        {
+            Console.Write($"Escolha um número de {minimo} a {maximo}: ");
+            string numeroEscolhido = Console.ReadLine()!;
+
+            if (int.Parse(numeroEscolhido) > numeroAleatorio)
+            {
+                Console.WriteLine("Número aleatório é menor!");
+            }
+            else if (int.Parse(numeroEscolhido) < numeroAleatorio)
+            {
+                Console.WriteLine("Número aleatório é maior!");
+            }
+            else
+            {
+                Console.WriteLine($"Parabéns, você acertou o número aleatório: {numeroAleatorio}");
+                break;
+            }
+        } while (true);
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Desafio do Jogo Aleatório em CSharp
